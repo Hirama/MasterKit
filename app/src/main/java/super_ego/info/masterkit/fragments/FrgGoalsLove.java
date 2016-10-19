7b0318ba4d86cd8ebf140f25fd0fd9605a62dbfe
Boburmirzo <bumurzaqov@mail.ru>
2016-10-19 11:20:19 +0300

    changed in goalLove

--- app/src/main/java/super_ego/info/masterkit/fragments/FrgGoalsLove.java
+++ app/src/main/java/super_ego/info/masterkit/fragments/FrgGoalsLove.java
@@ -1,5 +1,6 @@
 package super_ego.info.masterkit.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
