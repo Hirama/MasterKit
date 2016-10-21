ab21760ec068d365398f3cfce483c8d80144f036
a.zolin <a.zolin@innopolis.ru>
2016-10-21 23:13:04 +0300

    Add Youtube lib file and change git ignore

---
+++ app/src/main/java/super_ego/info/masterkit/fragments/learning_fragment/LearningStepFragment.java
@@ -0,0 +1,11 @@
+package super_ego.info.masterkit.fragments.learning_fragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by Andrey on 21.10.2016.
+ */
+
+public class LearningStepFragment extends Fragment {
+
+}
